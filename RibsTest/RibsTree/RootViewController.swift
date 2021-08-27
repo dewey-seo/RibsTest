11fbf7ba456ef5effd01d6a81fdd5d95f61ad599
dewey <dewey.seo@icloud.com>
2021-08-27 09:41:59 +0700

    feat: add Ribs file

---
+++ RibsTest/RibsTree/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  RibsTest
+//
+//  Created by dewey seo on 27/08/2021.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol RootPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class RootViewController: UIViewController, RootPresentable, RootViewControllable {
+
+    weak var listener: RootPresentableListener?
+}
